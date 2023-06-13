9fa6be708a7c502557c10e5013f10cb2fabfedd3
PabloJ4 <008pablo008@gmail.com>
2023-06-13 18:48:05 -0300

    exercicios M2

---
+++ Variaveis_e_Operacoes/exercicios_M2/ex_variaveis.py
@@ -0,0 +1,10 @@
+"""
+2) Faça um programa que receba o nome de um aluno e quanto ele tirou na prova de programação,
+depois imprima em apenas uma linha o nome no modo title() e quanto a pessoa tirou na prova.
+Ex: Ana Carolina tirou 8
+Soluçaõ:
+nome = input('Digite o nome da/o aluno/a: ') #Pede nome ao usuario
+nota = input('Digite a nota desse aluno/a: ') #Pede a nota ao usuario
+print(f'{nome.title()} tirou {nota}') #Imprime ambos com nome no modo title()
+"""
+
